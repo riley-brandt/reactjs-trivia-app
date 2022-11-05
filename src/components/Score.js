48d19c42f221ef55af65fa24a5547b4f81c30a96
johnny-pumpernickel <johnnypumpernickel@protonmail.com>
2022-11-05 17:40:46 -0600

    Score.js/end screen logic added

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { QuizContext } from "../helpers/Contexts";
+import { Questions } from "../helpers/Questions";
 
 function Score() {
-  return <div>EndScreen</div>;
+  const { score, setScore, setGameState } = useContext(QuizContext);
+  const restartQuiz = () => {
+    setScore(0);
+    setGameState("menu");
+  };
+
+  return (
+    <div className="end-screen">
+      <h1>Final Score</h1>
+      <h3>
+        {" "}
+        {score} / {Questions.length}{" "}
+      </h3>
+      <button onClick={restartQuiz}>Start Over</button>
+    </div>
+  );
 }
 
 export default Score;
