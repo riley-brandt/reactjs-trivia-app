65013b8218a7cd8e9d7077a454271d94259234b0
riley-brandt <rileyb3@proton.me>
2022-11-22 14:46:05 -0700

    Got button selection toggle to work by using a ternary operator with inline styles

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -11,13 +11,6 @@ function Quiz() {
   const [answerSelected, setAnswerSelected] = useState("");
   const [counter, setCounter] = useState(1);
 
-  // const nextQuestion = () => {
-  //   if (Questions[currentQ].answer == answerSelected) {
-  //     setScore(score + 1);
-  //   }
-  //   setCurrentQ(currentQ + 1);
-  // };
-
   const finishQuiz = () => {
     if (Questions[currentQ].answer == answerSelected) {
       setScore(score + 1);
@@ -37,41 +30,56 @@ function Quiz() {
     <div className="Quiz">
       <div className="counter-container">
         <span>
-          {" "}
           {counter} / {Questions.length}
         </span>
       </div>
+
       <h3>{Questions[currentQ].question}</h3>
+
       <div className="options">
         <button
           className="btn-quiz"
           onClick={() => setAnswerSelected("option1")}
+          style={{
+            backgroundColor:
+              answerSelected === "option1" ? "#ff60f7" : "#2f3857",
+          }}
         >
           {Questions[currentQ].option1}
         </button>
-        <br />
+
         <button
           className="btn-quiz"
           onClick={() => setAnswerSelected("option2")}
+          style={{
+            backgroundColor:
+              answerSelected === "option2" ? "#ff60f7" : "#2f3857",
+          }}
         >
           {Questions[currentQ].option2}
         </button>
-        <br />
+
         <button
           className="btn-quiz"
           onClick={() => setAnswerSelected("option3")}
+          style={{
+            backgroundColor:
+              answerSelected === "option3" ? "#ff60f7" : "#2f3857",
+          }}
         >
           {Questions[currentQ].option3}
         </button>
-        <br />
+
         <button
           className="btn-quiz"
           onClick={() => setAnswerSelected("option4")}
+          style={{
+            backgroundColor:
+              answerSelected === "option4" ? "#ff60f7" : "#2f3857",
+          }}
         >
           {Questions[currentQ].option4}
         </button>
-        <br />
-        <br />
       </div>
       <div className="btn-quiz-nav">
         {currentQ == Questions.length - 1 ? (
