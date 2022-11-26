7fceba318a814e03a89628dffb8743f9ee753686
riley-brandt <rileyb3@proton.me>
2022-11-26 16:13:34 -0700

    chaged inline style to className to fix button hover styles. Add navigation comments

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,12 +4,16 @@ import { QuizContext } from "../helpers/Contexts";
 
 import "./Quiz.css";
 
+// ========== STATES & CONTEXT ===========
+
 function Quiz() {
   const { gameState, setGameState } = useContext(QuizContext);
   const { score, setScore } = useContext(QuizContext);
   const [currentQ, setCurrentQ] = useState(0);
   const [answerSelected, setAnswerSelected] = useState("");
   const [counter, setCounter] = useState(1);
+
+  // ========= NAVIGATION BUTTONS ===========
 
   const handleFinishQuizBtn = () => {
     if (Questions[currentQ].answer === answerSelected) {
@@ -38,50 +42,56 @@ function Quiz() {
       <h3>{Questions[currentQ].question}</h3>
 
       <div className="options">
+        {/* ============= BUTTON 1 ============== */}
         <button
           className="btn-quiz"
           onClick={() => setAnswerSelected("option1")}
-          style={{
-            backgroundColor:
-              answerSelected === "option1" ? "#ff60f7" : "#2f3857",
-          }}
+          className={
+            answerSelected === "option1" ? "btn-quiz-selected" : "btn-quiz"
+          }
         >
           {Questions[currentQ].option1}
         </button>
 
+        {/* ============= BUTTON 2 ============== */}
+
         <button
           className="btn-quiz"
           onClick={() => setAnswerSelected("option2")}
-          style={{
-            backgroundColor:
-              answerSelected === "option2" ? "#ff60f7" : "#2f3857",
-          }}
+          className={
+            answerSelected === "option2" ? "btn-quiz-selected" : "btn-quiz"
+          }
         >
           {Questions[currentQ].option2}
         </button>
 
+        {/* ============= BUTTON 3 ============== */}
+
         <button
           className="btn-quiz"
           onClick={() => setAnswerSelected("option3")}
-          style={{
-            backgroundColor:
-              answerSelected === "option3" ? "#ff60f7" : "#2f3857",
-          }}
+          className={
+            answerSelected === "option3" ? "btn-quiz-selected" : "btn-quiz"
+          }
         >
           {Questions[currentQ].option3}
         </button>
 
+        {/* ============= BUTTON 4 ============== */}
+
         <button
           className="btn-quiz"
           onClick={() => setAnswerSelected("option4")}
-          style={{
-            backgroundColor:
-              answerSelected === "option4" ? "#ff60f7" : "#2f3857",
-          }}
+          className={
+            answerSelected === "option4" ? "btn-quiz-selected" : "btn-quiz"
+          }
         >
           {Questions[currentQ].option4}
         </button>
       </div>
+
+      {/* ============= NAVIGATION BUTTONS ============== */}
+
       <div className="btn-quiz-nav">
         {currentQ === Questions.length - 1 ? (
           <button onClick={handleFinishQuizBtn}>Finish Quiz</button>
